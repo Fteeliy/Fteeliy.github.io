226825ecf646636bf584956874d88dd733c81c13
8200802 <Fteeliy@yandex.ru>
2023-04-25 14:23:42 +0300

    Fixed

---
+++ BasketballScore/js/index.js
@@ -0,0 +1,35 @@
+function plusAndReset(whoScore, pannel, reset) {
+	let score = 0;
+
+	pannel.forEach((btn, i) => {
+		['touchstart', 'click'].forEach(function(k) {
+			btn.addEventListener(k, function () {
+				if (score < 99) {
+					switch (i) {
+						case 0: {
+							score += 1;
+							break;
+						}
+						case 1: {
+							score += 2;
+							break;
+						}
+						case 2: {
+							score += 3;
+							break;
+						}
+					}
+					whoScore.textContent = score;
+				}
+			})
+
+			reset.addEventListener(k, function () {
+				whoScore.textContent = 0;
+				score = 0;
+			});
+		})
+	});
+}
+
+plusAndReset(homeScore, homeBtns, resetHome);
+plusAndReset(guestScore, guestBtns, resetGuest);
