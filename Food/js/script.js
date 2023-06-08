ca696f2744b57c8546f355889bc275d160516791
Vladimir <Fteeliy@yandex.ru>
2023-06-08 13:52:09 +0300

    upd

--- Food/js/script.js
+++ Food/js/script.js
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
 	modal('[data-modal]', '.modal', modalTimerId);
-	timer('.timer', '2023-05-29');
+	timer('.timer', '2023-06-30');
 	cards();
 	calc();
 	forms('form', modalTimerId);
